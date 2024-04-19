3683247c91eda7d1c98a37e60c9ee65c59a31016
Ivan Bodnarash <bodnarash1999@gmail.com>
2024-04-19 00:00:06 -0600

    loading & displaying chat messages + styling chat bubbles

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,8 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,19 +10,25 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Flutter Chat'), actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-          )
-        ]),
-        body: const Center(
-          child: Text('Logged In!'),
-        ));
+      appBar: AppBar(title: const Text('Flutter Chat'), actions: [
+        IconButton(
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+          },
+          icon: Icon(
+            Icons.exit_to_app,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        )
+      ]),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ), // ChatMessages(),
+          NewMessage(),
+        ],
+      ),
+    );
   }
 }
